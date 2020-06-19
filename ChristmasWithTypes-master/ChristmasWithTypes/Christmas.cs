213a4adc65f6cd151bc38f98a23a958a746f3d13
harrisonpearson18 <66958157+harrisonpearson18@users.noreply.github.com>
2020-06-18 21:24:31 -0500

    Add files via upload

---
+++ ChristmasWithTypes-master/ChristmasWithTypes/Christmas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ChristmasWithTypes
+{
+    public class Christmas
+    {
+        public string[] Presents;
+        public string Santa { get; set; }
+        public int? TreeHeight { get; set; } 
+
+        
+        public enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+    }
+}
