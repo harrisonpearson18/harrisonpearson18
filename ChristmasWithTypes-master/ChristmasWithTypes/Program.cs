36e1d8eb43d5161fe2b5857cd17b12c24b9f23be
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-06-18 21:19:45 -0500

    ChristmasWithTypes Example

---
+++ ChristmasWithTypes-master/ChristmasWithTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChristmasWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmas = new Christmas();
+
+            var xmasDay = Christmas.Day.Thursday;
+
+            
+            xmas.Santa = "Kris Kringle";
+
+            
+            xmas.Presents =  new string[3]{"Xbox", "Playstation", "Computer" }; 
+
+            
+            xmas.TreeHeight = 10;
+
+            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
+            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
+            Console.WriteLine("Here are the presents we would like:");
+
+            foreach (var present in xmas.Presents)
+            {
+                Console.WriteLine($"    {present}");
+            }
+
+            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+        }
+    }
+}
