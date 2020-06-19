1c4bbf0858d8702504055557339c04cf5ffc96eb
harrisonpearson18 <66958157+harrisonpearson18@users.noreply.github.com>
2020-06-19 11:57:45 -0500

    Add files via upload
    A method creating exercise

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("First off, what is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your preferred color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite sport to watch?");
+            string sport = Console.ReadLine();
+            Console.WriteLine("What model of car do you like?");
+            string car = Console.ReadLine();
+
+            Console.WriteLine("Hey there, my name is " + (name) + " I am " + (age) + " years old and enjoy watching " + (sport) + " iny my free time.");
+            Console.WriteLine(carBuilder(color, car));
+
+
+
+        }
+
+        public static string carBuilder(string color, string car)
+        {
+
+            Console.WriteLine("My favorite car to take joyrides in is a " + color + " " + car);
+            return null;
+        }
+
+
+    }
+}
