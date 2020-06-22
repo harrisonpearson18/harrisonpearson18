3a817e39d4a698b11f9a8c01c3e7f355d5240166
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-06-22 14:57:23 -0500

    Intial Commit for SelectionStatements

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            Console.WriteLine($"Guess the correct number between 1 and 100");
+            var userInput = int.Parse(Console.ReadLine());
+            
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too low");
+            }
+
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine($"Too high");
+            }
+
+            else
+            {
+                Console.WriteLine($"Nevermind");
+            }
+
+            Console.WriteLine($"The correct number was {favNumber}");
+        }
+    }
+}
