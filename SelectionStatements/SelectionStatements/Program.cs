4629ff9a417744fa30e67c2e62bb76d7974ec753
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-06-23 11:32:35 -0500

    Added Switch statements to exercise

--- SelectionStatements/SelectionStatements/Program.cs
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -4,6 +4,8 @@ namespace SelectionStatements
 {
     class Program
     {
+        private const string V = "Math";
+
         static void Main(string[] args)
         {
             var r = new Random();
@@ -27,6 +29,36 @@ namespace SelectionStatements
             }
 
             Console.WriteLine($"The correct number was {favNumber}");
+
+            subjectSwitch();
+            subjectSwitch();
+        }
+        static void subjectSwitch()
+        {
+            Console.WriteLine("Input your favorite school subject from the list below.");
+            Console.WriteLine("Subjects to choose from: Math, Science, History, English, or Spanish");
+            string userSubject = Console.ReadLine().ToLower();
+        switch (userSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is a lot of fun");
+                    break;
+                case "science":
+                    Console.WriteLine("Lots can be learned from science");
+                    break;
+                case "history":
+                    Console.WriteLine("Knowing the past is important for the future");
+                    break;
+                case "english":
+                    Console.WriteLine("English is a hard language");
+                    break;
+                case "spanish":
+                    Console.WriteLine("Como estas?");
+                    break;
+                case null:
+                    Console.WriteLine("School is fun!");
+                    break;
+            }
         }
     }
 }
