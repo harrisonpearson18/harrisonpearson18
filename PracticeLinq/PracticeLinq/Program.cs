2493f4f1cd99db0349388e02c5fa80fc41152ff4
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-07-09 15:22:14 -0500

    Initial Commit of Linq Practice

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carNames = { "Mustang", "Corvette", "Ford", "GMC" };
+
+
+            var carSpaces = from car in carNames
+                            where car.Contains("r")
+                            orderby car ascending
+                            select car;
+
+            foreach (var car in carSpaces)
+            {
+                Console.WriteLine(car);
+            }
+
+            var games = new List<String> { "COD", "Valorant", "Dead by Daylight" };
+            IEnumerable<string> gameNameLength = games.OrderByDescending(x => x.Length);
+
+            foreach(var i in gameNameLength)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
