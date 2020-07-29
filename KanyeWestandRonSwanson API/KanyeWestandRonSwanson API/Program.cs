945bb9e54d6027220ab17252922e7b17fdc2b0c1
Harrison Pearson <harrison.pearson2000@gmail.com>
2020-07-28 19:39:49 -0500

    Ron&KanyeAPI initial commit

---
+++ KanyeWestandRonSwanson API/KanyeWestandRonSwanson API/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWestandRonSwanson_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    QuoteGenerator.KanyeQuote();
+                }
+                if (i % 2 == 1)
+                {
+                    QuoteGenerator.RonQuote();
+                }
+            }
+        }
+    }
+}
---
+++ KanyeWestandRonSwanson API/KanyeWestandRonSwanson API/QuoteGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWestandRonSwanson_API
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"===============");
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.WriteLine($"===============");
+
+        }
+
+        public static void RonQuote()
+        {
+            var client2 = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client2.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"==============");
+            Console.WriteLine($"Ron: {ronQuote}");
+            Console.WriteLine($"==============");
+        }
+
+
+    }
+}
